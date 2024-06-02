2acb446764068f8d3e1f4eb0aeb37e918bf064b0
sangwon432 <swonl0622@gmail.com>
2024-06-02 00:40:21 -0700

    map Blogs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+
+  const getBlogs = async () => {
+    try{
+      const {data, status} = await axios.get('http://localhost:8000/api/blog/all')
+      console.log(data)
+      setBlogs(data.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getBlogs()
+  }, []);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <br/>
+        {blogs?.map(blog => (
+            <div>
+              <h1>{blog.title}</h1>
+              <h2>{blog.category}</h2>
+              <h2>{blog.desc}</h2>
+              <h2>{blog.author}</h2>
+              <h2>{blog.image}</h2>
+            </div>
+        ))}
+      </div>
+  )
 }
 
 export default App;
